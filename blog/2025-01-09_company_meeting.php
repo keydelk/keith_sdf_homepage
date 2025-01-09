43a2d9ecd82a7790cacc4cd8d0425d0a21ced3e6
Keith Keydel <keydelk@gmail.com>
2025-01-09 07:46:47 -0700

    added company_meeting blog post

---
+++ blog/2025-01-09_company_meeting.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="UTF-8">
+    <!-- Add Title -->
+    <title>Company Meetings Quickly Drain My Social Batter</title>
+    <meta name="author" content="Keith Keydel">
+    <link rel="stylesheet" href="../css/simple.css">
+  </head>
+  <body>
+    <header>
+      <?php include "../nav.php" ?>
+
+      <!-- Include Title in H1-->
+      <h1>Company Meetings Quickly Drain My Social Battery</h1>
+    </header>
+    <main>
+      <!-- MAIN CONTENT HERE -->
+       <h2>My Introversion Gets More Pronounced as I get Older</h2>
+       <p>
+        No post yesterday, as I was dead tired by the time I got home and went 
+        to be immediately after dinner. We had our annual company meeting, which 
+        means that instead of normal work, we have a full day of all company 
+        meetings followed by a highly encouraged company happy hour. I don't 
+        particularly mind company social events, I'm lucky to have pretty good 
+        coworkers who I like working with. But I'm definitely more of an introvert, 
+        and hours of social interaction leave me feeling pretty drained.
+       </p>
+       <h2>Big Projects Ahead</h2>
+       <p>
+        I can't go into too much detail, as blog posts are public (even if almost nobody 
+        reads it), and the company hasn't publicly announced these upcoming features. 
+        But it sounds like my team is going to be working on a very prominent, user-facing 
+        feature. We've done similar projects before, but after months of minor enhancements,
+        bug fixes, and backend work like new APIs, I'm feeling a bit nervous about picking 
+        up such a big, user-facing feature.
+       </p>
+       <p>
+        I work as the Business Analyst on the team, so my responsibilities center 
+        around requirements gathering, some design work, writing user-stories in 
+        Jira, and some project-management type work. This upcoming feature is 
+        going to be a good test of my abilities.
+       </p>
+      <a href="index.php" class="button">&larr; Back to the blog</a>
+    </main>
+    <footer>
+      <p>Author: Keith Keydel</p>
+      <!-- DON'T FORGET TO UPDATE THE DATE (both datetime attribute and display value) -->
+      <p>Last Updated: <time datetime="2025-01-09">January 9, 2025</time></p>
+    </footer>
+  </body>
+</html>
